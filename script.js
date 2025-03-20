078739e060862f293eaac7f3e5f0e46f42df4b8a
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-03-20 13:10:45 -0400

    added function to generate computers choice

--- script.js
+++ script.js
@@ -6,18 +6,33 @@ console.log(choice)
 function getComputerChoice(){
 //randomly return one of the following string values: "Rock" "Paper" "Scissors"
 
-//let Rock equal 0, Paper equal 1, and scissors equal 2;  
-let rock = 0;
-let paper = 1;
-let scissors = 2;
-
 //pick equals random value between 0 and 1 rounded down, multiplied by 3
-let pick = Math.random() * 3;
+let pick = Math.floor((Math.random() * 3));
 
-//return pick value
-return pick;
+//assign the value a name, either Rock, Paper, or scissors
+  //if value equals 0, Rock
+  //if value equals 1, Paper
+  //if value equals 2, paper  
+switch(pick) {
+  
+  case 0:
+  pick = 'Rock'
+  break;
+
+  case 1:
+  pick = 'Paper'
+  break;
+
+  case 2:
+  pick = 'Scissors'
+  break;
+
+  default:
+  alert( 'Error, please refresh' );
 }
 
-
+//return the string that is generated 
+return pick;
+}
 
 //human choice
