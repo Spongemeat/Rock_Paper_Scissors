6c588ee6e11b2b3d5fcc5fb1b0d2e33d7d16ba6e
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-03-20 12:53:21 -0400

    first draft of logic pseudocode for getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+//computer choice
+let choice = getComputerChoice()
+
+console.log(choice)
+
+function getComputerChoice(){
+//randomly return one of the following string values: "Rock" "Paper" "Scissors"
+
+//let Rock equal 0, Paper equal 1, and scissors equal 2;  
+let rock = 0;
+let paper = 1;
+let scissors = 2;
+
+//pick equals random value between 0 and 1 rounded down, multiplied by 3
+let pick = Math.random() * 3;
+
+//return pick value
+return pick;
+}
+
+
+
+//human choice
