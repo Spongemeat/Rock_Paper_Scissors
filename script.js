765828c74bfc86b43af7d498893b621f405c37b0
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-04-16 13:51:05 -0400

    integrated computerchoice and playRound into button event listeners

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//initialize
+
 let playerChoice;
 let compChoice;
 
@@ -5,33 +7,32 @@ const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 
-//const rpsButton = document.querySelector('#btn');
-
+//button listeners
 rockButton.addEventListener('click', () =>{
   playerChoice = 'rock';
-  playGame()
+  compChoice = getComputerChoice();
+  playRound(playerChoice, compChoice);
 });
 
 paperButton.addEventListener('click', () =>{
   playerChoice = 'paper';
-  playGame()
+  compChoice = getComputerChoice();
+  playRound(playerChoice, compChoice);
 });
 
 scissorsButton.addEventListener('click', () =>{
   playerChoice = 'scissors';
-  playGame()
+  compChoice = getComputerChoice();
+  playRound(playerChoice, compChoice);
 });
 
-
-//!--RPScode--!//
-
+//div 
 //declare scores, starting at 0
 let playerScore = 0;
 let computerScore = 0;
 let round = 0;
 
-//console.log(playGame());
-//console.log(play);
+//!--RPScode--!//
 
 function getComputerChoice(){
   //pick equals random value between 0 and 1 rounded down, multiplied by 3
@@ -59,16 +60,11 @@ function getComputerChoice(){
   return pick;
 }
 
-//human choice
-function getPlayerChoice(){
-  let input = prompt('Type your pick: ');
-  input = input.toLowerCase();
-  
-  return input;
-}
 
 //play a round
 function playRound(playerChoice, compChoice){
+  round ++;
+
   let playerWin = 'Player wins ';
   let compWin = 'Computer wins ';
 
